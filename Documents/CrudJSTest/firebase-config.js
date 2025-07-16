fa47df14fda1673632f19ea8f58aa2fc1b6d58b3
Juanin348 <juanin348@gmail.com>
2025-07-16 09:42:36 +0100

    a

---
+++ Documents/CrudJSTest/firebase-config.js
@@ -0,0 +1,26 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDcgdYaerMLGRRXtx4u6GjpJR0T1FbkyjA",
+    authDomain: "test-firebase-a3703.firebaseapp.com",
+    projectId: "test-firebase-a3703",
+    storageBucket: "test-firebase-a3703.firebasestorage.app",
+    messagingSenderId: "467112888069",
+    appId: "1:467112888069:web:1dcf147bfb91c8dba7596c",
+    measurementId: "G-0Y29S95MRY"
+ };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app); // db es base de datos
+
+  export { db, analytics, app };
