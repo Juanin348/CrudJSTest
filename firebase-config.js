15411c0765250fd652c85323163cbe6d00b97290
Juanin348 <juanin348@gmail.com>
2025-10-15 10:11:14 +0100

    a

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
-  import { initializeFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,9 +27,6 @@
   } catch (_) {
     analytics = undefined;
   }
-  // Forzar/auto-detectar long polling para redes que rompen WebChannel (400 Bad Request)
-  const db = initializeFirestore(app, {
-    experimentalAutoDetectLongPolling: true
-  }); // db es base de datos
+  const db = getFirestore(app);
 
   export { db, analytics, app };
